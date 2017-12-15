bf31fa776a2968bdd04d11a07d7050c6cdc276e8
Tigran Ghahramanyan <tghahram@gmail.com>
2017-12-15 15:18:34 +0400

    nlohmann json is downloaded automatically and is available as json target with cmake

--- src/mesh.pp/global.hpp
+++ src/mesh.pp/global.hpp
@@ -2,3 +2,11 @@
 
 #define MESH_EXPORT BELT_EXPORT
 #define MESH_IMPORT BELT_IMPORT
+
+#ifdef B_OS_UNIX
+#define M_OS_UNIX
+#endif
+
+#ifdef B_OS_WINDOWS
+#define M_OS_WINDOWS
+#endif
--- src/test_app/main.cpp
+++ src/test_app/main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
 #include <belt.pp/message.hpp>
+#include <json/json.hpp>
 
 int main()
 {
-   //auto test = 1;
-   beltpp::message msg;
-   std::cout << msg.type() << std::endl;
-   return 0;
+    //auto test = 1;
+    nlohmann::json var = {{"name", "niels"},
+                          {"last_name","lohmann"}};
+    beltpp::message msg;
+    std::cout << msg.type() << std::endl;
+    std::cout << var << std::endl;
+    return 0;
 }
