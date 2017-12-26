32c3393d200bddfd4f93322ff239321d02692e16
Tigran Ghahramanyan <tghahram@gmail.com>
2017-12-27 00:41:36 +0400

    sync with belt.pp basic parser

--- src/test_app/main.cpp
+++ src/test_app/main.cpp
@@ -28,10 +28,7 @@ class message_blockchain_issue_block :
 
 size_t a = beltpp::typelist::type_list_index<event_network_transaction_call, event_list>::value;
 
-using discard_lexers =
-beltpp::typelist::type_list<
-class white_space_lexer
->;
+
 
 using operator_lexers =
 beltpp::typelist::type_list<
@@ -41,25 +38,11 @@ class operator_minus_lexer
 //class operator_divide
 >;
 
-using value_lexers =
-beltpp::typelist::type_list<
-class value_string_lexer,
-class value_number_lexer
-//class value_bool,
-//class value_null
->;
-
 using scope_lexers =
 beltpp::typelist::type_list<
 class scope_braces_lexer
 //class scope_parentheses
 >;
-
-class white_space_lexer :
-        public beltpp::discard_lexer_base<white_space_lexer,
-                                            discard_lexers,
-                                            beltpp::standard_white_space_set>
-{};
 
 class operator_plus_lexer :
         public beltpp::operator_lexer_base<operator_plus_lexer,
@@ -67,6 +50,7 @@ class operator_plus_lexer :
                                             beltpp::standard_operator_set>
 {
 public:
+    enum { grow_priority = 1 };
     bool final_check(std::string::iterator it_begin,
                      std::string::iterator it_end) const
     {
@@ -79,121 +63,11 @@ class operator_minus_lexer :
                                             beltpp::standard_operator_set>
 {
 public:
+    enum { grow_priority = 0 };
     bool final_check(std::string::iterator it_begin,
                      std::string::iterator it_end) const
     {
         return std::string(it_begin, it_end) == "-";
-    }
-};
-
-class value_string_lexer :
-        public beltpp::value_lexer_base<value_string_lexer, value_lexers>
-{
-    enum states
-    {
-        state_none = 0x0,
-        state_single_quote_open = 0x01,
-        state_double_quote_open = 0x02,
-        state_escape_symbol = 0x04
-    };
-    int state = state_none;
-    size_t index = -1;
-public:
-    std::pair<bool, bool> check(char ch)
-    {
-        ++index;
-        if (0 == index && ch == '\'')
-        {
-            state |= state_single_quote_open;
-            return std::make_pair(true, false);
-        }
-        if (0 == index && ch == '\"')
-        {
-            state |= state_double_quote_open;
-            return std::make_pair(true, false);
-        }
-        if (0 == index && ch != '\'' && ch != '\"')
-            return std::make_pair(false, false);
-        if (0 != index && ch == '\'' &&
-            (state & state_single_quote_open) &&
-            !(state & state_escape_symbol))
-            return std::make_pair(true, true);
-        if (0 != index && ch == '\"' &&
-            (state & state_double_quote_open) &&
-            !(state & state_escape_symbol))
-            return std::make_pair(true, true);
-        if (0 != index && ch == '\\' &&
-            !(state & state_escape_symbol))
-        {
-            state |= state_escape_symbol;
-            return std::make_pair(true, false);
-        }
-        state &= ~state_escape_symbol;
-        return std::make_pair(true, false);
-    }
-};
-
-double stod(std::string const& value, size_t& pos)
-{
-    double result;
-    try
-    {
-        result = std::stod(value, &pos);
-    }
-    catch(...)
-    {
-        pos = 0;
-    }
-
-    return result;
-}
-uint64_t stoull(std::string const& value, size_t& pos)
-{
-    uint64_t result = 0;
-    try
-    {
-        result = std::stoull(value, &pos);
-    }
-    catch(...)
-    {
-        pos = 0;
-    }
-
-    return result;
-}
-
-class value_number_lexer :
-        public beltpp::value_lexer_base<value_number_lexer, value_lexers>
-{
-    size_t state_dot_count = 0;
-public:
-    std::pair<bool, bool> check(char ch)
-    {
-        std::string str_full_set = "0123456789.elx";
-        if (str_full_set.find(ch) != std::string::npos)
-        {
-            if (0 == state_dot_count && ch == '.')
-                state_dot_count = 1;
-            else if (ch == '.')
-                return std::make_pair(false, false);
-
-            return std::make_pair(true, false);
-        }
-        return std::make_pair(false, false);
-    }
-
-    bool final_check(std::string::iterator it_begin,
-                     std::string::iterator it_end) const
-    {
-        size_t pos = 0;
-        std::string value(it_begin, it_end);
-        stod(value, pos);
-        if (pos != value.length())
-            stoull(value, pos);
-
-        if (pos != value.length())
-            return false;
-        return true;
     }
 };
 
@@ -237,23 +111,18 @@ public:
 
 int main()
 {
-    int aa = 0;
-    using token_type = beltpp::typelist::type_list_get<1, operator_lexers>::type;
-    token_type tt;
-    operator_minus_lexer tt1;
-    tt1 = tt;
-    ++aa;
     std::unique_ptr<
             beltpp::expression_tree<operator_lexers,
-                                    value_lexers,
+                                    beltpp::standard_value_lexers,
                                     scope_lexers,
-                                    discard_lexers,
-                                    std::string>> a;
-    std::string test("\'+-\'+0x2 + s{- 0x3}\"str\\\"ing\'\"-\'k\\\'\"l\'");
+                                    beltpp::standard_discard_lexers,
+                                    std::string>> ptr_expression;
+    //std::string test("\'+-\'+0x2 + s{- 0x3}\"str\\\"ing\'\"-\'k\\\'\"l\'");
+    std::string test("{\'192.168.1.1\'+\'13\'}");
     auto it_begin = test.begin();
     auto it_end = test.end();
     auto it_begin_keep = it_begin;
-    while (beltpp::parse(a, it_begin, it_end))
+    while (beltpp::parse(ptr_expression, it_begin, it_end))
     {
         if (it_begin == it_begin_keep)
             break;
@@ -263,12 +132,59 @@ int main()
             it_begin_keep = it_begin;
         }
     }
-    //auto test = 1;
-    nlohmann::json var = {{"name", "niels"},
-                          {"last_name","lohmann"}};
-    beltpp::message msg;
-    //msg.set();
-    std::cout << msg.type() << std::endl;
-    std::cout << var << std::endl;
+
+    if (nullptr == ptr_expression)
+        return 0;
+
+    while (auto parent = ptr_expression->parent)
+    {
+        ptr_expression.release();
+        ptr_expression.reset(parent);
+    }
+
+    auto p_iterator = ptr_expression.get();
+    std::vector<size_t> track;
+    size_t depth = 0;
+
+    while (p_iterator)
+    {
+        if (track.size() == depth)
+            track.push_back(0);
+
+        if (track.size() == depth + 1)
+        {
+            for (size_t index = 0; index != depth; ++index)
+                std::cout << ' ';
+            std::cout << p_iterator->lexem.value << std::endl;
+        }
+
+        size_t next_child_index = -1;
+        if (false == p_iterator->children.empty())
+        {
+            size_t childindex = 0;
+            if (track.size() > depth + 1)
+            {
+                ++track[depth + 1];
+                childindex = track[depth + 1];
+            }
+
+            if (childindex < p_iterator->children.size())
+                next_child_index = childindex;
+        }
+
+        if (size_t(-1) != next_child_index)
+        {
+            p_iterator = p_iterator->children[next_child_index];
+            ++depth;
+            if (track.size() > depth + 1)
+                track.resize(depth + 1);
+        }
+        else
+        {
+            p_iterator = p_iterator->parent;
+            --depth;
+        }
+    }
+
     return 0;
 }
