46ab3d75be5fafdfa210311fd75124f9fcd4c047
Tigran Ghahramanyan <tghahram@gmail.com>
2018-05-19 18:56:50 +0400

    use actual pid in the lock file

--- src/libsystemutility/fileutility.cpp
+++ src/libsystemutility/fileutility.cpp
@@ -1,6 +1,7 @@
 #include "fileutility.hpp"
 
 #include "file_attributes.hpp"
+#include "processutility.hpp"
 
 #include <belt.pp/utility.hpp>
 
@@ -16,10 +17,10 @@ namespace detail
 {
 int create_lock_file(boost::filesystem::path& path)
 {
-    int fd = open(path.native().c_str(), O_RDWR | O_CREAT, 0666);
+    int fd = ::open(path.native().c_str(), O_RDWR | O_CREAT, 0666);
     if (fd >= 0 && flock(fd, LOCK_EX | LOCK_NB))
     {
-        close(fd);
+        ::close(fd);
         fd = -1;
     }
     return fd;
@@ -27,7 +28,7 @@ int create_lock_file(boost::filesystem::path& path)
 bool write_to_lock_file(int native_handle, std::string const& value)
 {
     if (value.length() !=
-        (size_t)write(native_handle, value.c_str(), value.length()))
+        (size_t)::write(native_handle, value.c_str(), value.length()))
         return false;
     return true;
 }
@@ -35,13 +36,13 @@ void delete_lock_file(int native_handle, boost::filesystem::path& path)
 {
     if (native_handle < 0)
         return;
-    remove(path.native().c_str());
-    close(native_handle);
+    ::remove(path.native().c_str());
+    ::close(native_handle);
 }
 void dostuff(int native_handle, boost::filesystem::path const& path)
 {
     FileAttributes::LockedByPID attr_lock;
-    attr_lock.pid = 11;
+    attr_lock.owner = current_process_id();
     beltpp::packet pck_lock(std::move(attr_lock));
     FileAttributes::Attributes attrs;
     attrs.attributes.push_back(std::move(pck_lock));
