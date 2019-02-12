6695234ea77328e9d0eada4c4592b7332ec21ea1
Tigran Ghahramanyan <tghahram@gmail.com>
2019-02-12 20:29:28 +0400

    reusable code moved to meshpp

---
+++ src/libsystemutility/settings.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "global.hpp"
+
+#include <boost/filesystem.hpp>
+
+#include <string>
+#include <memory>
+
+namespace meshpp
+{
+SYSTEMUTILITYSHARED_EXPORT boost::filesystem::path config_directory_path();
+SYSTEMUTILITYSHARED_EXPORT boost::filesystem::path data_directory_path();
+
+SYSTEMUTILITYSHARED_EXPORT void create_config_directory();
+SYSTEMUTILITYSHARED_EXPORT void create_data_directory();
+
+SYSTEMUTILITYSHARED_EXPORT boost::filesystem::path config_file_path(std::string const& file);
+SYSTEMUTILITYSHARED_EXPORT boost::filesystem::path data_file_path(std::string const& file);
+SYSTEMUTILITYSHARED_EXPORT boost::filesystem::path data_directory_path(std::string const& dir);
+
+
+class SYSTEMUTILITYSHARED_EXPORT settings
+{
+public:
+    static void set_data_directory(std::string const& data_dir);
+    static std::string data_directory();
+
+    static void set_application_name(std::string const& application_name);
+    static std::string application_name();
+private:
+    static std::string s_data_dir;
+    static std::string s_application_name;
+};
+}// end of namespace meshpp
---
+++ src/mesh.pp/settings.hpp
@@ -0,0 +1,2 @@
+#pragma once
+#include "../libsystemutility/settings.hpp"
